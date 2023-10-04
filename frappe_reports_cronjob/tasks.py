4def8d0edc6eee09746c4ce90a56085bcb067d6d
ahmedtouahria <ahmedtouahria2001@gmail.com>
2023-10-04 23:44:44 +0100

    add all functions

--- frappe_reports_cronjob/tasks.py
+++ frappe_reports_cronjob/tasks.py
@@ -1,17 +1,76 @@
 import frappe
 from frappe.utils import now_datetime
+from frappe_reports_cronjob.utils import *
 
 def send_sales_finance_report():
     # Compose the email content
-    email_content = 'Test email'
     # Replace 'recipient@example.com' with the recipient's email address
     recipient_email = "ahmedtouahria2001@gmail.com"
     print("send_sales_finance_report")
+    unpaid=get_total_unpaid_invoices()
+    average_payment= get_average_payment_time()
+    get_total_payments=get_total_payments_received_last_month()
+    calculate_net_profit = calculate_net_profit_of_month()
+    chiffre_d_affaires_du_mois = calculate_chiffre_d_affaires_du_mois()
+    profit_margin = calculate_profit_margin()
+    get_total_sales_count=get_total_sales_count_this_month()
+    get_total_sales_amount=get_total_sales_amount_this_month()
+    get_new_customers_count=get_new_customers_count_this_month()
+    payments_received=get_total_payments_received()
+    unpaid_invoices=get_total_unpaid_invoices()
+    email_content = f'profit_margin = {profit_margin} % get_average_payment_time = {average_payment},get_total_unpaid_invoices = {unpaid},get_total_payments = {get_total_payments} , calculate_net_profit ={calculate_net_profit} , chiffre_d_affaires_du_mois ={chiffre_d_affaires_du_mois}'
+    email_content+=f"get_total_sales_count = {get_total_sales_count} , get_total_sales_amount = {get_total_sales_amount} , get_new_customers_count = {get_new_customers_count} "
+    email_content+=f"payments_received = {payments_received} , unpaid_invoices = {unpaid_invoices} "
+
+    print(email_content)
     # Send the email
-    frappe.sendmail(
+    """ frappe.sendmail(
         recipients=[recipient_email],
         sender=None,  # Use the default sender configured in Frappe
         subject="Sales Finance Report",
         message=email_content,
         
-    )
+    ) """
+
+def send_stock_report():
+    # Compose the email content
+    recipient_email = "ahmedtouahria2001@gmail.com"
+    print("send_stock_report")
+    initial_stock_count = get_initial_stock_count()
+    #total_orders_value = get_total_orders_value()
+
+    email_content = f"initial_stock_count = {initial_stock_count} \n"
+    #email_content += f"total_orders_value = {total_orders_value}"
+
+    print(email_content)
+    # Send the email
+    """ frappe.sendmail(
+        recipients=[recipient_email],
+        sender=None,  # Use the default sender configured in Frappe
+        subject="Sales Finance Report",
+        message=email_content,
+        
+    ) """
+
+def send_production_report():
+    # Compose the email content
+    recipient_email = "ahmedtouahria2001@gmail.com"
+    print("send_production_report")
+    total_products_manufactured = get_total_products_manufactured()
+    total_production_cost = get_total_production_cost()
+
+    email_content = f"total_products_manufactured = {total_products_manufactured} \n"
+    email_content += f"total_production_cost = {total_production_cost}"
+
+    print(email_content)
+    # Send the email
+    """ frappe.sendmail(
+        recipients=[recipient_email],
+        sender=None,  # Use the default sender configured in Frappe
+        subject="Sales Finance Report",
+        message=email_content,
+        
+    ) """
+
+
+# produ
--- frappe_reports_cronjob/utils.py
+++ frappe_reports_cronjob/utils.py
@@ -1,39 +1,57 @@
 import frappe
 from frappe.utils import now_datetime
 from datetime import datetime
-
-def calculate_turnover_of_month():
-    current_month = frappe.utils.now_datetime().strftime("%Y-%m")
-    turnover = frappe.db.sql("""
-        SELECT SUM(amount) FROM `tabSalesInvoice`
-        WHERE docstatus = 1 AND posting_date LIKE %s
-    """, ("%{}%".format(current_month)))
-
-    return turnover[0][0] if turnover else 0
+from frappe.utils import get_datetime
+from frappe.utils import nowdate, add_days, get_first_day, get_last_day
 
 
+def calculate_chiffre_d_affaires_du_mois():
+    current_datetime = now_datetime()
+    start_date = get_first_day(current_datetime)
+    end_date = get_last_day(current_datetime)
+
+    total_revenue = frappe.get_all("Sales Invoice", filters={
+        "posting_date": ["between", [start_date, end_date]],
+        "docstatus": 1  # Include only submitted documents
+    }, fields=["SUM(grand_total) as total_revenue"])[0].total_revenue or 0
+
+    return total_revenue
 
 def calculate_net_profit_of_month():
-    current_month = now_datetime().strftime("%Y-%m")
-    revenue = frappe.db.sql("""
-        SELECT SUM(amount) FROM `tabSalesInvoice`
-        WHERE docstatus = 1 AND posting_date LIKE %s
-    """, ("%{}%".format(current_month)))
+    """
+    Calculate the net profit of the current month.
 
-    expenses = frappe.db.sql("""
-        SELECT SUM(amount) FROM `tabExpense`
-        WHERE docstatus = 1 AND posting_date LIKE %s
-    """, ("%{}%".format(current_month)))
+    Returns:
+        float: The net profit of the current month.
+    """
 
-    return (revenue[0][0] if revenue else 0) - (expenses[0][0] if expenses else 0)
+    start_date = frappe.utils.get_datetime().replace(day=1)
+    end_date = frappe.utils.get_datetime().replace(day=31)
 
+    total_revenue = frappe.get_all("Sales Invoice", filters={
+        "posting_date": ["between", (start_date, end_date)]
+    }, fields=["total"])
+    print(total_revenue)
+    total_expenses = frappe.get_all("Purchase Invoice", filters={
+        "posting_date": ["between", (start_date, end_date)]
+    }, fields=["total"])
+    print(total_expenses)
+    sum_a = sum(item['total'] for item in total_revenue)
 
+# Calculate the sum of 'total' values in list 'b'
+    sum_b = sum(item['total'] for item in total_expenses)
+
+    # Subtract the sum of 'b' from the sum of 'a'
+    result = sum_a - sum_b
+
+    return result
 def calculate_profit_margin():
     net_profit = calculate_net_profit_of_month()
-    turnover = calculate_turnover_of_month()
+    turnover = calculate_chiffre_d_affaires_du_mois()
 
     if turnover != 0:
-        return (net_profit / turnover) * 100
+        result =(net_profit / turnover) * 100
+        return float(result)
     else:
         return 0
 
@@ -69,22 +87,36 @@ def get_new_customers_this_month():
     return new_customers
 
 
-def get_total_payments_received_this_month():
-    current_month = datetime.now().strftime("%Y-%m")
-    total_payments_received = frappe.get_all(
-        "Payment Entry",
-        filters={"docstatus": 1, "posting_date": ("like", f"{current_month}%")},
-        fields=["SUM(paid_amount) as total_payments"],
-    )[0].total_payments
-    return total_payments_received
+def get_total_payments_received_last_month():
+    """
+    Return the total payments received in the last month.
+    """
 
+    current_date = nowdate()
+    last_month_start = get_first_day(add_days(current_date, -30))
+    last_month_end = get_last_day(add_days(current_date, -1))
+
+    payments = frappe.get_all("Payment Entry", filters={
+        "payment_type": "Receive",
+        "posting_date": ["between", (last_month_start, last_month_end)]
+    })
+
+    total_payments = 0
+
+    for payment in payments:
+        payment_entry = frappe.get_doc("Payment Entry", payment["name"])
+        if payment_entry and payment_entry.paid_amount_after_tax:
+            total_payments += payment_entry.paid_amount_after_tax
+
+    return total_payments
 def get_total_unpaid_invoices():
     total_unpaid_invoices = frappe.get_all(
         "Sales Invoice",
         filters={"docstatus": 1, "outstanding_amount": (">", 0)},
         fields=["SUM(outstanding_amount) as total_unpaid"],
-    )[0].total_unpaid
-    return total_unpaid_invoices
+    )
+    unpaid = total_unpaid_invoices[0].total_unpaid
+    return unpaid if unpaid is not None else 0
 
 
 
@@ -94,4 +126,132 @@ def get_average_payment_time():
         filters={"docstatus": 1, "due_date": (">", frappe.utils.now())},
         fields=["AVG(DATEDIFF(due_date, posting_date)) as avg_payment_time"],
     )[0].avg_payment_time
-    return avg_payment_time
+    return avg_payment_time if avg_payment_time is not None else 0
+
+
+
+# indicateur des clients
+
+def get_total_sales_count_this_month():
+    current_datetime = now_datetime()
+    start_date = get_first_day(current_datetime)
+    end_date = get_last_day(current_datetime)
+
+    sales_invoices = frappe.get_all("Sales Invoice", filters={
+        "posting_date": ["between", [start_date, end_date]],
+        "docstatus": 1  # Include only submitted documents
+    })
+
+    total_sales_count = len(sales_invoices)
+
+    return total_sales_count
+
+def get_total_sales_amount_this_month():
+    current_datetime = now_datetime()
+    start_date = get_first_day(current_datetime)
+    end_date = get_last_day(current_datetime)
+
+    total_sales_amount = frappe.get_all("Sales Invoice", filters={
+        "posting_date": ["between", [start_date, end_date]],
+        "docstatus": 1  # Include only submitted documents
+    }, fields=["SUM(grand_total) as total_sales_amount"])[0].total_sales_amount or 0
+
+    return total_sales_amount
+
+def get_new_customers_count_this_month():
+    current_datetime = now_datetime()
+    start_date = get_first_day(current_datetime)
+    end_date = get_last_day(current_datetime)
+
+    new_customers = frappe.get_all("Customer", filters={
+        "creation": ["between", [start_date, end_date]]
+    })
+
+    new_customers_count = len(new_customers)
+
+    return new_customers_count
+
+# payment
+
+def get_total_payments_received():
+    # Get the sum of paid amounts from Payment Entry documents
+    total_payments = frappe.get_all("Payment Entry", filters={
+        "docstatus": 1  # Include only submitted documents
+    }, fields=["SUM(paid_amount) as total_payments"])[0].total_payments or 0
+
+    return total_payments
+
+
+def get_total_unpaid_invoices():
+    # Get the sum of outstanding amounts from Sales Invoice documents
+    total_unpaid_invoices = frappe.get_all("Sales Invoice", filters={
+        "docstatus": 1,  # Include only submitted documents
+        "outstanding_amount": (">", 0)  # Filter for invoices with outstanding amounts
+    }, fields=["SUM(outstanding_amount) as total_unpaid_invoices"])[0].total_unpaid_invoices or 0
+
+    return total_unpaid_invoices
+
+
+#============ stock ==================
+
+def get_initial_stock_count():
+    # Replace "Item" with the appropriate DocType representing your inventory items
+    stock_entries = frappe.get_all("Stock Ledger Entry", filters={
+        "posting_date": ("<", get_first_day(frappe.utils.now_datetime())),
+        "docstatus": 1  # Include only submitted documents
+    }, fields=["SUM(actual_qty) as initial_stock_count"])
+
+    return stock_entries[0].initial_stock_count or 0
+
+def get_final_stock_count():
+    # Replace "Item" with the appropriate DocType representing your inventory items
+    stock_entries = frappe.get_all("Stock Entry", filters={
+        "posting_date": ("<=", get_last_day(frappe.utils.now_datetime())),
+        "docstatus": 1  # Include only submitted documents
+    }, fields=["SUM(actual_qty) as final_stock_count"])
+
+    return stock_entries[0].final_stock_count or 0
+
+
+#Indicateurs d'Approvisionnement:
+
+def get_total_orders_count():
+    # Replace "Purchase Order" with the appropriate DocType representing your purchase orders
+    orders = frappe.get_all("Purchase Order", filters={
+        "docstatus": 1  # Include only submitted documents
+    })
+
+    return len(orders)
+
+def get_total_orders_value():
+    # Replace "Purchase Order" with the appropriate DocType representing your purchase orders
+    orders = frappe.get_all("Purchase Order", filters={
+        "docstatus": 1  # Include only submitted documents
+    }, fields=["SUM(grand_total) as total_orders_value"])
+
+    return orders[0].total_orders_value or 0
+
+
+# production
+
+def get_total_products_manufactured():
+    # Replace "Production Order" with the appropriate DocType representing your production orders
+    production_orders = frappe.get_all("Production Order", filters={
+        "status": "Completed",
+        "start_date": ["between", (get_first_day(frappe.utils.now_datetime()), get_last_day(frappe.utils.now_datetime()))]
+    })
+
+    total_products = sum(order.qty for order in production_orders)
+
+    return total_products
+
+def get_total_production_cost():
+    # Replace "Production Order" with the appropriate DocType representing your production orders
+    production_orders = frappe.get_all("Production Order", filters={
+        "status": "Completed",
+        "start_date": ["between", (get_first_day(frappe.utils.now_datetime()), get_last_day(frappe.utils.now_datetime()))]
+    })
+
+    total_cost = sum(order.total_cost for order in production_orders)
+
+    return total_cost
